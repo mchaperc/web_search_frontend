2a123089a27fff71feffb41a5da025e066272986
Matt Chastain <matthewdchastain@gmail.com>
2017-04-08 15:15:34 -0400

    bare-bones frontend functionality

--- search.js
+++ search.js
@@ -1,13 +1,25 @@
 let currentLocation = window.location.href.slice(window.location.href.indexOf('=') + 1);
 
+let searchText = document.getElementById('searchText');
+searchText.innerText = decodeURIComponent(currentLocation);
+
+let resultsContainer = document.getElementById('resultsContainer');
+
 marmottajax({
     url: `https://basic-search-engine.herokuapp.com/search/${currentLocation}`,
     method: 'get',
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-    },
 }).success(function(result) {
-    console.log(result);
+    let urls = JSON.parse(result);
+    console.log(urls);
+    let tempFn = doT.template(
+    	`
+    		{{~it.urls :value:index}}
+    			<a href="{{=value.url}}" class="panel">{{=value.url}}</a>
+    		{{~}}
+    	`
+    )
+    console.log(tempFn({urls}))
+    resultsContainer.innerHTML = tempFn({urls});
 }).error(function(message) {
     console.log(message);
 });
