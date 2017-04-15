b6bf8eb848f0147c122f745ab6e1b608455116e2
Matt Chastain <matthewdchastain@gmail.com>
2017-04-14 20:42:55 -0400

    mods to results

--- search.js
+++ search.js
@@ -6,7 +6,8 @@ searchText.innerText = decodeURIComponent(currentLocation);
 let resultsContainer = document.getElementById('resultsContainer');
 
 marmottajax({
-    url: `https://basic-search-engine.herokuapp.com/search/${currentLocation}`,
+    // url: `https://basic-search-engine.herokuapp.com/search/${currentLocation}`,
+    url: `http://127.0.0.1:8000/search/${currentLocation}`,
     method: 'get',
 }).success(function(result) {
     let urls = JSON.parse(result);
@@ -14,7 +15,13 @@ marmottajax({
     let tempFn = doT.template(
     	`
     		{{~it.urls :value:index}}
-    			<a href="{{=value.url}}" class="panel">{{=value.url}}</a>
+    		    <div class="panel">
+    		        <a href="{{=value.url}}">{{=value.title || value.url}}
+    		            <p>{{=value.url}}</p>
+    		            <p>{{=value.description || ''}}</p>
+    		        </a>
+                </div>
+    			
     		{{~}}
     	`
     )
