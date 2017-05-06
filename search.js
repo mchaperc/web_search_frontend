4db8ebf395fcdf6f55ebdb0b067be5bdacf596e8
Matt Chastain <matthewdchastain@gmail.com>
2017-05-06 18:39:17 -0400

    pagination partially functioning; search added to results page

--- search.js
+++ search.js
@@ -5,15 +5,23 @@ searchText.innerText = decodeURIComponent(currentLocation);
 
 let resultsContainer = document.getElementById('resultsContainer');
 
-marmottajax({
-    url: `https://basic-search-engine.herokuapp.com/search/${currentLocation}`,
-    // url: `http://127.0.0.1:8000/search/${currentLocation}`,
-    method: 'get',
-}).success(function(result) {
-    let urls = JSON.parse(result);
-    console.log(urls);
-    let tempFn = doT.template(
-    	`
+let pageNum = 1;
+
+let searchQuery = () => {
+    marmottajax({
+        url: `https://basic-search-engine.herokuapp.com/search/${currentLocation}`,
+        // url: `http://127.0.0.1:8000/search/${currentLocation}/${pageNum}`,
+        method: 'get',
+    }).success(function(result) {
+        result = JSON.parse(result);
+        let urls = result && result.search_results;
+        let pages = [];
+        let pageNums = (result && result.pages) || 10;
+        for (let i = 0; i < pageNums; i++) {
+            pages.push(i+1);
+        }
+        let tempFn = doT.template(
+            `
     		{{~it.urls :value:index}}
     		    <div class="panel">
     		        <a href="{{=value.url}}">{{=value.title || value.url}}
@@ -23,10 +31,30 @@ marmottajax({
                 </div>
     			
     		{{~}}
+    		<div class="pagination">
+    		    <a href="#"><i class="fa fa-angle-left"></i></a>
+    		    {{~it.pages :value:index}}
+    		        <a href="#" id={{=value}} class="page-link {{? value == pageNum}} active {{?}}">{{=value}}</a><!--
+    		        -->
+    		    {{~}}
+    		    <a href="#"><i class="fa fa-angle-right"></i></a>
+            </div>
     	`
-    )
-    console.log(tempFn({urls}))
-    resultsContainer.innerHTML = tempFn({urls});
-}).error(function(message) {
-    console.log(message);
-});
+        )
+        resultsContainer.innerHTML = tempFn({urls, pages});
+        window.scrollTo(0, 0);
+        let pageLinks = document.getElementsByClassName('page-link');
+        [...pageLinks].forEach( ( pageLink ) => {
+            pageLink.onclick = ( e ) => {
+                e.preventDefault();
+                pageNum = pageLink.id;
+                searchQuery();
+            };
+        });
+        document.getElementById('searchFormInput').value = currentLocation.replace(/%20/gi, ' ');
+    }).error(function(message) {
+        console.log(message);
+    });
+}
+
+searchQuery();
